4b38a46c39aaae83145b7d01a819a708e2b8421f
reefle1001 <147424278+reefle1001@users.noreply.github.com>
2023-11-09 01:02:52 +0200

    Add files via upload

---
+++ src/ask01.java
@@ -0,0 +1,6 @@
+class ask01 {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("I am going to learn Java!");
+    }
+}
---
+++ src/ask02.java
@@ -0,0 +1,6 @@
+public class ask02 {
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+        System.out.print("I am going to learn Java!");
+    }
+}
