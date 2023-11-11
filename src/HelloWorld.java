4750686ee26036d11651c26526063caabb17fbd9
reefle1001 <147424278+reefle1001@users.noreply.github.com>
2023-11-11 15:44:30 +0200

    Add files via upload

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=3){
+            System.out.println("Hellow World!");
+            i++;}
+    }
+}
---
+++ src/helloLoop.java
@@ -0,0 +1,6 @@
+public class helloLoop {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++)
+            System.out.println("Hello World!");
+    }
+}
