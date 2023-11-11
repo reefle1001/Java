eb1b098e36a96c351c82d75d3a79062f2deb4bbe
reefle1001 <147424278+reefle1001@users.noreply.github.com>
2023-11-11 15:43:54 +0200

    Delete src directory

--- src/ask01.java
+++
@@ -1,6 +0,0 @@
-class ask01 {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-        System.out.println("I am going to learn Java!");
-    }
-}
--- src/ask03.java
+++
@@ -1,6 +0,0 @@
-public class ask03 {
-    public static void main(String[] args){
-        System.out.print("Hello World!\n");
-        System.out.print("I am going to learn Java!\n");
-    }
-}
