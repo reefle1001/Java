44742c164d111b64d09c9d670a737989b8f91be7
reefle1001 <147424278+reefle1001@users.noreply.github.com>
2023-12-13 23:37:52 +0200

    Delete src directory

--- src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        int i=1;
-        while(i<=3){
-            System.out.println("Hellow World!");
-            i++;}
-    }
-}
--- src/helloLoop.java
+++
@@ -1,6 +0,0 @@
-public class helloLoop {
-    public static void main(String[] args) {
-        for(int i=1;i<=3;i++)
-            System.out.println("Hello World!");
-    }
-}
