daba592b7d2edd556baf72da3cc7ae3ab149e3cc
reefle1001 <147424278+reefle1001@users.noreply.github.com>
2023-12-14 03:02:35 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        STATS s1;
+        s1 = new STATS();
+        //s1.ShowAllStableStats();
+        //s1.ShowPlayerStats();
+        //s1.HP =1000;
+
+        s1.Input_Armor_Stats("5",6,4,5,3,5);
+        //s1.ShowPlayerStats();
+
+
+        //s1.ST_ArSt();
+    }
+}
